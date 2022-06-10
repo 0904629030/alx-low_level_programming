f4833a8e9342586e641cc80cee998b10a95ffb8d
shishay <sishailsh0@gmail.com>
2022-06-10 06:40:19 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-int main(void)
-{
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 
-	return (0);}
+
+/**
+ *
+ *   * main - Prints a text
+ *
+ *     *
+ *
+ *       * Return: Always 0 (Success)
+ *
+ *         */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
